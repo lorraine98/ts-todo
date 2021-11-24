8db415353179872fa84dbc892c1d8f023e5e32d2
lorraine98 <hyosung0425@naver.com>
2021-11-24 12:10:07 +0900

    add task comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header>ToDo</Header>
+    </div>
+  );
 }
 
 export default App;
